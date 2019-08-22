250aa6cb1116d43ca2245d7ebfc9007b23e410ae
jason <jaszhix@gmail.com>
2019-08-22 16:58:31 -0500

    Fix the stored list not updating after manual location registration

--- app/modals.tsx
+++ app/modals.tsx
@@ -298,6 +298,7 @@ export class LocationRegistrationModal extends React.Component<LocationRegistrat
         ...location
       }).then((res) => {
         state.trigger('fetchRemoteLocations');
+        state.trigger('markStoredLocationsDirty');
         this.handleClickOutside();
       }).catch((err) => {
         this.setState({
