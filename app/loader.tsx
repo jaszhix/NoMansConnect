33ec3e894cd8e140f5e1cd450888dec9310dd4c8
jason <jaszhix@gmail.com>
2020-01-17 17:46:57 -0600

    Loader: migrate inline styles

--- app/loader.tsx
+++ app/loader.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import {map} from '@jaszhix/utils';
 
-const textStyle: CSSProperties = {
-  position: 'relative',
-  top: '140px',
-  fontSize: '28px',
-  width: '300px'
-};
-
 const fill: number[] = Array(64).fill('');
 
 interface LoaderProps {
@@ -16,25 +9,25 @@ interface LoaderProps {
 
 class Loader extends React.Component<LoaderProps> {
   render() {
-    let style: CSSProperties = !this.props.loading ? {
-      fontSize: '6px',
-      left: '0px',
-      position: 'relative'
-    } : null;
+    const {loading} = this.props;
+
     return (
-      <div className="loader" style={style}>
+      <div className={`loader${loading ? '' : ' initial'}`}>
+
         {map(fill, (v, i)=>{
           return (
             <span key={i} />
           );
         })}
+
         <div className="pulsing">
           <div className="circle" />
           <div className="circle" />
           <div className="circle" />
           <div className="circle" />
         </div>
-        {this.props.loading ? <div style={textStyle}>{this.props.loading}</div> : null}
+
+        {loading ? <div className="text">{this.props.loading}</div> : null}
       </div>
     );
   }
